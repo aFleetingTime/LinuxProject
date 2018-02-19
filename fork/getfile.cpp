ad02d48e6d7e95a7df4e1c1ca8f75bd6b70f52e9
Liunian <liunian320@outlook.com>
2018-02-19 14:34:54 +0800

    update

--- fork/getfile.cpp
+++ fork/getfile.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	ofstream ofs("./file.txt", ios::out | ios::trunc);
 	random_device random;
-	int num = random() % 1024000 + 102400;
+	int num = random() % 10240000 + 1024000;
 	for(int i = 0; i < num; ++i)
 		ofs << ('a' + random() % 26);
 	ofs.close();
